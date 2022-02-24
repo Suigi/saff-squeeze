97982b9f75261b3093d24fcb5a1571f417a18655
Daniel Ranner <daniel.ranner@jollydays.com>
2022-02-24 17:41:09 +0100

    Autopilot is wasting fuel, added a test for it

---
+++ src/test/java/ninja/ranner/saffsqueeze/AutopilotTest.java
@@ -0,0 +1,46 @@
+package ninja.ranner.saffsqueeze;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AutopilotTest {
+
+    @Test
+    public void autopilot_flies_to_closest_runway() {
+        var aircraft = new FakeAircraft(Coordinates.of(2, 2));
+        var closestLondonRunway = Runway.at(5, 5);
+        var furtherLondonRunway = Runway.at(10, 10);
+        var london = new Airport("London", closestLondonRunway, furtherLondonRunway);
+        var autopilot = new Autopilot(aircraft, london);
+
+        autopilot.flyTo("London");
+
+        aircraft.assertTargetCoordinatesAre(Coordinates.of(5, 5));
+    }
+
+    public static class FakeAircraft implements Autopilot.Aircraft {
+
+        private final Coordinates currentPosition;
+        private Coordinates target;
+
+        public FakeAircraft(Coordinates currentPosition) {
+            this.currentPosition = currentPosition;
+        }
+
+        @Override
+        public void setWayPoint(Coordinates plan) {
+            this.target = plan;
+        }
+
+        @Override
+        public Coordinates currentPosition() {
+            return currentPosition;
+        }
+
+        public void assertTargetCoordinatesAre(Coordinates coordinates) {
+            assertThat(this.target)
+                    .isEqualTo(coordinates);
+        }
+    }
+}
